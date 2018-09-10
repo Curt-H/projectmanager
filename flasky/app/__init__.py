8e82dac90dd41ad1a7ea9b4fe7744175958d4cc9
Curt-H <Curt_H@126.com>
2018-09-10 16:01:38 +0800

    Add: a flask demo
    Just make sure the new structrue can wo rk well

--- flasky/app/__init__.py
+++ flasky/app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flasky.app.util import log
+from flasky.app.main import main as main_route
 
 
 def create_app():
     app = Flask(__name__)
-
+    app.register_blueprint(main_route)
     return app
 
 
@@ -12,8 +13,8 @@ if __name__ == '__main__':
     app_test = create_app()
     config = dict(
         debug=True,
-        host='0.0.0.0',
+        host='localhost',
         port=80,
     )
 
-    app_test.run(config)
+    app_test.run(**config)
