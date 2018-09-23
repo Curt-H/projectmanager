7990c96594b914f0f43dc80ece47edbd231b429c
Curt_H <Curt_H@126.com>
2018-09-23 22:16:47 +0800

    Add: demo page about adding new task

--- flasky/app/main/public.py
+++ flasky/app/main/public.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+import json
+
+from flask import Blueprint, render_template, request
 
 from flasky.app import log
 from flasky.app.models.task import Task
@@ -16,3 +18,15 @@ def task_now():
     ts = Task.all()
     log(tasks=ts)
     return str(ts)
+
+
+@public.route('/new', methods=['GET'])
+def task_new_view():
+    return render_template('task_new.html')
+
+
+@public.route('/new', methods=['POST'])
+def task_new_add():
+    msg = json.dumps(request.form)
+    log(msg)
+    return msg
