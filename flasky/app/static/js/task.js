e2babc35e63ffa4b10a68c95c80786854a69b7e8
Curt_H <Curt_H@126.com>
2018-09-28 23:46:10 +0800

    Add - JS finish fuction demo

--- flasky/app/static/js/task.js
+++ flasky/app/static/js/task.js
@@ -1,34 +1,27 @@
 let apiTaskAll = function (callback) {
-    let path = '/api/todo/all'
+    let path = '/api/todo/all';
     ajax('GET', path, '', callback)
-}
+};
 
-let taskTemplate = function (task) {
-    let template = `
-        
-    `
-}
+let bindEvenFinish = function () {
+    let taskList = e('#task-list');
 
-let insertTask = function (task) {
-    let taskPanel = taskTemplate(task)
-    let taskList = e('#task-list')
-    log('task list', taskList)
-    taskList.insertAdjacentHTML('beforeend', taskPanel)
-}
+    // Event trusteeship
+    taskList.addEventListener('click', function (t) {
+        let self = t.target
+        log('The object clicked:', self)
 
-let loadTasks = function () {
-    apiTaskAll(function (tasks) {
-        log('redy to load all tasks')
-        for (i = 0; i < tasks.length; i++) {
-            let task = tasks[i]
-            log('task object', task)
-            insertTask(todo)
-        }
+        // Get task panel object
+        let taskPanel = self.closest('.panel')
+        log('The closest object:', taskPanel)
+        let taskId = taskPanel.dataset['Id']
+        log('Task ID:', taskId)
     })
-}
+
+};
 
 let __main = function () {
-    loadTasks()
-}
+    bindEvenFinish();
+};
 
-__main()
+__main();
