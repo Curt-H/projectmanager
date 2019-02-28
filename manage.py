1845dd425ec498e753253164a763eebbac985e20
Curt the Guardian <guardian.curt@outlook.com>
2019-02-28 17:04:18 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     app = create_app()
     config = dict(
         debug=True,
-        host='localhost',
+        host='0.0.0.0',
         port=80,
     )
     app.run(**config)
