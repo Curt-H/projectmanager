963c64e1b2e7b1c37a898ab21bf6fe9be937d58a
Curt the Guardian <guardian.curt@outlook.com>
2019-03-15 09:18:53 +0800

    Update task.js

--- flasky/app/static/js/task.js
+++ flasky/app/static/js/task.js
@@ -31,20 +31,20 @@ let bindEventFinish = function () {
 
             let responseMsg = confirm('Really want to mark it as finished?\nYou can not reedit if you click yes');
             if (responseMsg) {
-                log('Sending data')
+                log('Sending data');
                 apiTaskFinish(data, function (response) {
-                    log(response)
+                    log(response);
 
                     // Change button clickable status
-                    let btn_finish = e('.finish', taskPanel)
-                    log('FINISH: ', btn_finish)
-                    btn_finish.classList.remove('pure-button-primary')
-                    btn_finish.classList.add('pure-button-disabled')
+                    let btn_finish = e('.finish', taskPanel);
+                    log('FINISH: ', btn_finish);
+                    btn_finish.classList.remove('pure-button-primary');
+                    btn_finish.classList.add('pure-button-disabled');
 
-                    let btn_cancle = e('.cancle', taskPanel)
-                    log('CANCLE:', btn_cancle)
-                    btn_cancle.classList.remove('pure-button-disabled')
-                    btn_cancle.classList.add('pure-button-primary')
+                    let btn_cancle = e('.cancle', taskPanel);
+                    log('CANCLE:', btn_cancle);
+                    btn_cancle.classList.remove('pure-button-disabled');
+                    btn_cancle.classList.add('pure-button-primary');
 
                     // Add <s> into text
                     taskIdTitle.innerHTML = '<s>' + taskIdTitle.innerHTML + '</s>';
