c4d9002529969da6b8b3745869d49f13a263be5f
Curt the Guardian <guardian.curt@outlook.com>
2019-04-29 09:54:16 +0800

    Feat: task deleted func -- js part

--- flasky/app/static/js/task.js
+++ flasky/app/static/js/task.js
@@ -3,6 +3,11 @@ let apiTaskFinish = function (data, callback) {
     ajax('POST', path, data, callback)
 };
 
+let apiTaskDelete = function (data, callback) {
+    let path = '/api/task/delete';
+    ajax('POST', path, data, callback)
+};
+
 // Dealing the finish button event
 let bindEventFinish = function () {
     let taskList = e('#task-list');
@@ -42,7 +47,7 @@ let bindEventFinish = function () {
 
                     // Add <s> into text
                     taskIdTitle.classList.add('fin');
-                    taskTitle.classList.add('fin')
+                    taskTitle.classList.add('fin');
                     RemoveItemWithAnime(taskPanel);
                 })
             }
@@ -60,10 +65,21 @@ let bindEventRemove = function () {
         // Get task panel object
         log(self.classList);
         if (self.classList.contains('cancle')) {
-            log('The object clicked:', self);
 
-            let taskPanel = self.closest('.panel');
-            RemoveItemWithAnime(taskPanel)
+            let responseMsg = confirm('DELETED?');
+            if (responseMsg) {
+                let task = self.closest('.panel');
+                let task_id = task.dataset['id'];
+
+                log('The object clicked:', self);
+                RemoveItemWithAnime(task);
+
+                let data = {
+                    id: task_id,
+            };
+                apiTaskDelete(data)
+            }
+
         }
     })
 };
