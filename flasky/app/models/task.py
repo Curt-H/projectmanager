2ee0f8bd8c55aba16cd1d73d4639dedd3e2387f0
FFF000 <guardian.curt@outlook.com>
2019-08-01 16:07:50 +0800

    Update task.py
    start refactor programme for the new company and more compitable to usural life

--- flasky/app/models/task.py
+++ flasky/app/models/task.py
@@ -4,8 +4,9 @@ from flasky.app.models import BaseModel
 class Task(BaseModel):
     def __init__(self, form):
         super().__init__(form)
-        self.project = form.get('project', '其他')
-        self.task_content = form.get('task_content', 'None')
-        self.creat_time = form.get('creat_time', '')  # an int variable
-        self.deadline = form.get('deadline', '未指定')  # an int variable
+        self.pn = form.get('pn', None)
+        self.type = form.get('type', None)
+        self.task_content = form.get('task_content', None)
+        self.creat_time = form.get('creat_time', None)  # an int variable
+        self.deadline = form.get('deadline', None)  # an int variable
         self.finish = form.get('finish', False)
