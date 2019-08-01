6157469be54e0ab6fe1cbfde55588513904d09ff
FFF000 <guardian.curt@outlook.com>
2019-08-01 17:33:00 +0800

    Feature: add login check

--- flasky/app/main/__init__.py
+++ flasky/app/main/__init__.py
@@ -6,8 +6,11 @@ But I recommended just add some tools here
 import time
 from functools import wraps
 
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for, request
 import json
+
+from flasky.app import log
+from flasky.app.models.commom_model import Session, User
 
 
 def convert_to_strtime(form):
@@ -21,11 +24,30 @@ def convert_to_strtime(form):
     return time.mktime(strtime)
 
 
+def current_user():
+    if 'session_id' in request.cookies:
+        session_id = request.cookies.get('session_id', 'guest')
+        session = Session.find_by(session_id=session_id)
+        if session is not None:
+            user_id = session.user_id
+            user = User.find_by(id=user_id)
+            return user
+        else:
+            return None
+    else:
+        return None
+
+
 def login_required(f):
     @wraps(f)
     def wrapper(*args):
-        print('This a deco demo')
-        return f(*args)
+        log('Check weather logged')
+
+        u = current_user()
+        if u is None:
+            return f(u)
+        else:
+            return f(u)
 
     return wrapper
 
--- flasky/app/main/public.py
+++ flasky/app/main/public.py
@@ -13,71 +13,78 @@ public = Blueprint('public', __name__)
 
 @public.route('/', methods=['GET'])
 @login_required
-def index():
-    a = 0
-    # ts = Task.all()
-    # for t in ts:
-    #     log(t.deadline)
-    #     log(type(t.deadline))
-    #     t.deadline = format_time(t.deadline, '[%Y-%m-%d-%a]')
-    #     log(t.deadline)
+def index(user=None):
+    u = user
+    if u is None:
+        u = {
+            'username': 'GUEST',
+        }
+        # ts = Task.all()
+        # for t in ts:
+        #     log(t.deadline)
+        #     log(type(t.deadline))
+        #     t.deadline = format_time(t.deadline, '[%Y-%m-%d-%a]')
+        #     log(t.deadline)
+        #
+        # tasks = list()
+        # projects = get_project_list()
+        # for p in projects:
+        #     for t in ts:
+        #         if t.project == p:
+        #             tasks.append(t)
+        #
+        # config = load_config()
+        # v = random.randint(1, 10086)
+        #
+        return render_template('index.html', u=u, a='Hello world')
+
+
+@public.route('/error', methods=['GET'])
+def log_in():
+    return 'WRONG'
+
+    # def load_config():
+    #     with open("config.ini", 'r', encoding='utf8') as f:
+    #         config = json.load(f)
+    #     log(config=config)
+    #     return config
+
+    # def save_config(config):
+    #     with open("config.ini", 'w', encoding='utf8') as f:
+    #         json.dump(config, f)
+    #     log(config=config)
+
+    # @public.route('/new', methods=['GET'])
+    # def task_new_view():
+    #     project = get_project_list()
     #
-    # tasks = list()
-    # projects = get_project_list()
-    # for p in projects:
-    #     for t in ts:
-    #         if t.project == p:
-    #             tasks.append(t)
+    #     log(project=project)
+    #     return render_template('task_new.html', project=project)
     #
-    # config = load_config()
-    # v = random.randint(1, 10086)
     #
-    return render_template('index.html')
-
-# def load_config():
-#     with open("config.ini", 'r', encoding='utf8') as f:
-#         config = json.load(f)
-#     log(config=config)
-#     return config
-
-
-# def save_config(config):
-#     with open("config.ini", 'w', encoding='utf8') as f:
-#         json.dump(config, f)
-#     log(config=config)
-
-
-# @public.route('/new', methods=['GET'])
-# def task_new_view():
-#     project = get_project_list()
-#
-#     log(project=project)
-#     return render_template('task_new.html', project=project)
-#
-#
-# @public.route('/new', methods=['POST'])
-# def task_new_add():
-#     form = json.loads(json.dumps(request.form))
-#     form['deadline'] = convert_to_strtime(form)
-#     form['creat_time'] = time.time()
-#     log(form, type(form))
-#
-#     Task.new(form)
-#     return redirect(url_for('.index'))
-#
-#
-# @public.route('/showhide', methods=['GET'])
-# def task_show_hide():
-#     config = load_config()
-#     config['show_finished_item'] = not config['show_finished_item']
-#     save_config(config)
-#
-#     return redirect(url_for('public.index'))
-#
-#
-# def get_project_list():
-#     tasks = Task.all()
-#     project = set()
-#     for t in tasks:
-#         project.add(t.project)
-#     return project
+    # @public.route('/new', methods=['POST'])
+    # def task_new_add():
+    #     form = json.loads(json.dumps(request.form))
+    #     form['deadline'] = convert_to_strtime(form)
+    #     form['creat_time'] = time.time()
+    #     log(form, type(form))
+    #
+    #     Task.new(form)
+    #     return redirect(url_for('.index'))
+    #
+    #
+    # @public.route('/showhide', methods=['GET'])
+    # def task_show_hide():
+    #     config = load_config()
+    #     config['show_finished_item'] = not config['show_finished_item']
+    #     save_config(config)
+    #
+    #     return redirect(url_for('public.index'))
+    #
+    #
+    # def get_project_list():
+    #     tasks = Task.all()
+    #     project = set()
+    #     for t in tasks:
+    #         project.add(t.project)
+    #     return project
