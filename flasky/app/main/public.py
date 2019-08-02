44cd3340110ba178401cc7e59752c9129b948583
FFF000 <guardian.curt@outlook.com>
2019-08-02 15:28:26 +0800

    Feature: login and validate page

--- flasky/app/main/public.py
+++ flasky/app/main/public.py
@@ -4,6 +4,7 @@ import time
 
 from flask import Blueprint, render_template, request, redirect, url_for
 from flasky.app.main import convert_to_strtime, login_required, response
+from flasky.app.models.commom_model import User
 from flasky.app.util import log, format_time
 
 # from flasky.app.models.commom_model import Task
@@ -26,3 +27,17 @@ def index(user=None):
 @public.route('/log_in', methods=['GET'])
 def log_in():
     return render_template('log_in.html')
+
+
+@public.route('/validate_login', methods=['POST'])
+def validate_login():
+    un = request.form.get('username')
+    pw = request.form.get('password')
+
+    user = User.find_by(username=un)
+    if user is None:
+        return 'NO SUCH USER'
+    elif pw != user.password:
+        return 'WRONG PASSWORD'
+    else:
+        return redirect(url_for('public.index', u=user))
